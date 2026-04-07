b2c2412a4e2db8c80d2fd78c0fed52fd3d9960c1
Luis Angel <luiecrzsotelo@gmail.com>
2026-04-07 01:49:17 -0600

    commit nlp token huggingface

--- src/nlp.py
+++ src/nlp.py
@@ -1,9 +1,10 @@
 from transformers import pipeline
+import os
 
 emotion_model = pipeline(
     "text-classification",
     model="mrm8488/distilbert-base-multilingual-cased-finetuned-emotion",
-    top_k=None
+    top_k=None,
     use_auth_token=os.environ.get("HUGGINGFACE_TOKEN")
 )
 

b0560c5f4347c7b7d5e640eaefb585ebc2dadc6e
Luis Angel <luiecrzsotelo@gmail.com>
2026-04-07 01:39:48 -0600

    commit

--- src/nlp.py
+++ src/nlp.py
@@ -1,29 +1,22 @@
 from transformers import pipeline
 
-emotion_model = None  # inicializamos como None
-
-def get_emotion_model():
-    global emotion_model
-    if emotion_model is None:
-        # Se carga solo la primera vez
-        emotion_model = pipeline(
-            "text-classification",
-            model="mrm8488/distilbert-base-multilingual-cased-finetuned-emotion",
-            top_k=None
-        )
-    return emotion_model
+emotion_model = pipeline(
+    "text-classification",
+    model="mrm8488/distilbert-base-multilingual-cased-finetuned-emotion",
+    top_k=None
+)
 
 def extract_emotions(text):
-    model = get_emotion_model()
-
-    result = model(
+    result = emotion_model(
         text,
         truncation=True,
         max_length=300
     )[0]
 
-    scores = {item["label"].lower(): item["score"] for item in result}
+    scores = {item["label"]: item["score"] for item in result}
+
     all_emotions = ["surprise", "neutral", "disgust", "anger", "joy", "fear", "sadness"]
+
     for e in all_emotions:
         scores.setdefault(e, 0)
 

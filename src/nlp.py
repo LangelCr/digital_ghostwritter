39d18eb53593d6e692aac89b6ec491e2f4bedfa8
Luis Angel <luiecrzsotelo@gmail.com>
2026-04-07 01:19:53 -0600

    commit

--- src/nlp.py
+++ src/nlp.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 
 emotion_model = pipeline(
     "text-classification",
-    model="j-hartmann/emotion-multi-language-distilbert",
+    model="j-hartmann/emotion-english-distilroberta-base",
     top_k=None
 )
 
